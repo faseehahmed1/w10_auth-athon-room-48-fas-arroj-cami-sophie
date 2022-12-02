329e138dc373345376311c54e9d900541ba79c73
camillabrizzi <camillabrizzi@icloud.com>
2022-12-02 14:44:58 +0000

    added text fields to card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,76 +1,93 @@
 import React, { useEffect, useState } from "react";
 import Card from "../src/Card";
 import { useAuth0 } from "@auth0/auth0-react";
-import Profile from "../src/Profile";
+import Profile from "./Profile";
 import signGenerator from "./signGenerator";
 import Button from "./Button";
 
 import "./App.css";
 
 function App() {
-	const { isAuthenticated } = useAuth0();
-	const [sign, setSign] = useState("");
-	const [day, setDay] = useState("today");
-	const [birthDay, setBirthDay] = useState("");
-	const [birthMonth, setBirthMonth] = useState("");
-	const [horoscope, setHoroscope] = useState({});
+  const { isAuthenticated } = useAuth0();
+  const [sign, setSign] = useState("");
+  const [day, setDay] = useState("today");
+  const [birthDay, setBirthDay] = useState("");
+  const [birthMonth, setBirthMonth] = useState("");
+  const [horoscope, setHoroscope] = useState({});
 
-	useEffect(() => {
-		let horoscopeSign = signGenerator(+birthDay, birthMonth);
-		setSign(horoscopeSign);
-	}, [birthDay, birthMonth]);
+  useEffect(() => {
+    let horoscopeSign = signGenerator(+birthDay, birthMonth);
+    setSign(horoscopeSign);
+  }, [birthDay, birthMonth]);
 
-	function handleClick(e) {
-		console.log(`You have clicked me!`);
-		async function getHoroscope() {
-			const rawResponse = await fetch(
-				`https://aztro.sameerkumar.website?sign=${sign}&day=${day}`,
-				{
-					method: "POST",
-					headers: {
-						Accept: "application/json",
-						"Content-Type": "application/json",
-					},
-				}
-			);
-			const content = await rawResponse.json();
-			//console.log(content);
+  function handleClick(e) {
+    console.log(`You have clicked me!`);
+    async function getHoroscope() {
+      const rawResponse = await fetch(
+        `https://aztro.sameerkumar.website?sign=${sign}&day=${day}`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const content = await rawResponse.json();
+      //console.log(content);
 
-			setHoroscope({
-				dateRange: content.date_range,
-				day: content.current_date,
-				description: content.description,
-				compatibility: content.compatibility,
-				mood: content.mood,
-				colour: content.color,
-				luckyNumber: content.lucky_number,
-				luckyTime: content.lucky_time,
-			});
-		}
-		getHoroscope();
-		console.log(horoscope);
-	}
+      setHoroscope({
+        dateRange: content.date_range,
+        day: content.current_date,
+        description: content.description,
+        compatibility: content.compatibility,
+        mood: content.mood,
+        colour: content.color,
+        luckyNumber: content.lucky_number,
+        luckyTime: content.lucky_time,
+      });
+    }
+    getHoroscope();
+    console.log(horoscope);
+  }
 
-	return (
-		<div className='App'>
-			<p>Hello!</p>
-			<Profile setBirthDay={setBirthDay} setBirthMonth={setBirthMonth} />
-			{isAuthenticated && (
-				<>
-					<Card>
-						<p>This is your horoscope</p>
-						<p>{birthDay}</p>
-						<p>{birthMonth}</p>
-					</Card>
-					<Button
-						text='Click to see your horoscope!'
-						handleClick={handleClick}
-						className='generate-horoscope'
-					/>
-				</>
-			)}
-		</div>
-	);
+  return (
+    <div className="App">
+      <header>Your Horoscope</header>
+      <Profile setBirthDay={setBirthDay} setBirthMonth={setBirthMonth} />
+      {isAuthenticated && (
+        <>
+          <Card>
+            <p>This is your horoscope</p>
+            <h1>{sign}</h1>
+            <h2>{horoscope.day}</h2>
+            <p>{horoscope.description}</p>
+            <p>
+              Your mood is: <span>{horoscope.mood}</span>
+            </p>
+            <p>
+              Your colour is: <span>{horoscope.colour}</span>
+            </p>
+            <p>
+              Your lucky number is: <span>{horoscope.luckyNumber}</span>
+            </p>
+            <p>
+              Your lucky time is: <span>{horoscope.luckyTime}</span>
+            </p>
+            <p>
+              Your most compatible sign is:
+              <span>{horoscope.compatibility}</span>
+            </p>
+          </Card>
+          <Button
+            text="Click to see your horoscope!"
+            handleClick={handleClick}
+            className="generate-horoscope"
+          />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
