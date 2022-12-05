dd5c7f62df6a63e6e44e364f97afaf492abdd10f
Faseeh Ahmed <faseeh_458@hotmail.com>
2022-12-05 16:39:35 +0000

    File names capitalised

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Card from "../src/Card";
+import Card from './Card'
 import { useAuth0 } from "@auth0/auth0-react";
 import Profile from "./Profile";
 import signGenerator from "./signGenerator";
